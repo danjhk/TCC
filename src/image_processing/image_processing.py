2d66be49b73f252a66ff3be010aff4e274f34de8
Daniel Kim <daniel.jin.kim@usp.br>
2020-04-24 12:23:40 -0300

    Finished doxygen configuration

---
+++ src/image_processing/image_processing.py
@@ -0,0 +1,264 @@
+''' Implementa funções de processamento de imagens
+@package image_processing
+
+@author Daniel Kim & Igor Nakamura
+@date Date: 2020-04-17
+
+@verbatim
+Funções presente no módulo: 
+
+count_frames        Conta a quantidade de frames de um vídeo
+show_frames         Mostra o conteúdo de um vídeo ao usuário
+frame_stacker       Agrupa os frames de um vídeo em listas
+preprocess          Reduz a escala dos frames e subtrai o seu background
+grayscale           Retorna frames em tons de cinza
+compute_gradient    Calcula os gradientes na direção X e Y de frames
+optical_flow        Calcula os fluxos ópticos na direção X e Y de frames
+@endverbatim 
+'''
+
+
+# -----------------------------
+# Standard library dependencies
+# -----------------------------
+from typing import List, Dict
+
+# -------------------
+# Third-party imports
+# -------------------
+import cv2
+
+def count_frames(video_cap):
+    """ Conta a quantidade de frames de um video
+
+    Args:
+        video_cap: objeto VideoCapture
+
+    @return num_frames: inteiro contendo a quantidade de frames
+
+    Examples:
+        >>> count_frames(video_cap)
+        '84'
+    """
+    cap = video_cap
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+    else:
+        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        return num_frames
+
+def show_frames(video_cap):
+    """ Mostra o conteúdo de um conjunto de frames ao usuário
+
+    Args:
+        video_cap: objeto VideoCapture
+    
+    @return None
+    
+    Examples:
+        >>> show_frames(video_cap)
+    """
+    
+    cap = video_cap
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2_imshow(frame)
+        # Pressione 'Q' no teclado para sair
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+        else: 
+            break
+    # Libera o objeto VideoCapture
+    cap.release()
+    # Fecha todos os frames
+    cv2.destroyAllWindows()
+
+def frame_stacker(video_cap, frames_per_stack):
+    """ Agrupa os frames de um vídeo em listas que contém uma 
+    quantidade de "frames_per_stack" de frames
+
+    Args:
+        video_cap: sequência de frames a ser agrupada
+        frames_per_stack: quantidade de frames por lista
+    
+    @return stacked_frames_list: lista de listas de imagens, 
+        correspondendo aos frames de um vídeo
+    
+    Examples:
+        >>> frame_stacker(video_cap, 9)
+    """
+    
+    frames_list = []
+    while(video_cap.isOpened()):
+    # Captura frame por frame
+      ret, frame = video_cap.read()
+      if ret == True:
+        frames_list.append(frame)    
+      else: 
+        break
+        
+    num_frames = len(frames_list)
+    num_stacks = num_frames - frames_per_stack + 1
+    first_frame_of_nth_stack = 0
+    last_frame_of_nth_stack = frames_per_stack  
+    stacked_frames_list = []
+
+    for n in range(num_stacks): # n representa o enésimo stack
+      stacked_frames_list.append(
+          frames_list[first_frame_of_nth_stack:last_frame_of_nth_stack])
+      first_frame_of_nth_stack += 1
+      last_frame_of_nth_stack += 1
+    return stacked_frames_list
+
+def preprocess(stacked_frames_list, scale):
+    """ Essa função realiza o pré-processamento para um conjunto de frames. 
+    Especificamente, reduz a escala dos frames e subtrai o seu background.
+    <br>
+    Args:
+        stacked_frames_list: uma lista de imagens, correspondendo
+            aos frames de um vídeo
+        scale: inteiro que indica a escala a ser aplicada no redimensionamento
+            dos frames. Ex: 2, para escalar a imagem para a metade do tamanho original
+
+    @return stack_list: lista de imagens, com os frames na escala
+        especificada e com foreground extraído
+    """
+
+    stack_list = [] # lista para armazenar os frames processados
+
+    for stacked_frames in stacked_frames_list: # itera para cada stack de frames
+      backSub = cv2.createBackgroundSubtractorMOG2(detectShadows = False,
+                                                varThreshold = 5) # Gaussian mixture model para
+                                                                        # extração do background
+      frame_list = [] # lista para armazenar os frames pré-processados
+
+      for frame in stacked_frames: # itera para cada frame dentro do stack
+
+        height , width, channels =  frame.shape
+        dim = (int(height/scale), int(width/scale))
+        resize = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA) # downsampling
+
+        fgMask = backSub.apply(resize) # aplica o foregorund extractor
+        fgMask3 = cv2.cvtColor(fgMask, cv2.COLOR_GRAY2RGB) # transforma máscara p 3 canais RGB
+
+        new_frame = cv2.bitwise_and(resize, fgMask3) # aplica operador lógico AND bit a bit (pixel a pixel)
+
+        cv2_imshow(fgMask3) # máscara
+        cv2_imshow(new_frame) # frame reduzido e com máscara aplicada
+        frame_list.append(new_frame) # adiciona o frame no stack
+
+      stack_list.append(frame_list) # adiciona o stack na lista
+    
+    return stack_list
+
+def grayscale(stacked_frames_list):
+    """ Esta função recebe uma lista com frames empilhados e retorna listas 
+    contendo os frames agrupados em stacks, em tons de cinza
+
+    Args:
+        stacked_frames_list: uma lista de imagens, correspondendo
+            aos frames de um vídeo
+        
+    @return stack_grayscale: lista de imagens, com os frames em escalas de cinza
+    """    
+    stacked_grayscale = [[cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY) \
+                            for frame in stacked_frames] \
+                            for stacked_frames in stacked_frames_list]      
+
+def compute_gradient(stacked_frames_list):
+    """ Esta função recebe uma lista com frames empilhados e retorna 
+    listas contendo os frames agrupados em stacks, com seus 
+    gradientes na direção X e Y
+    
+    Args:
+        stacked_frames_list: uma lista de imagens, correspondendo aos frames de um vídeo
+    
+    Multiple return: 
+        stacked_gradient_x_list: uma lista de listas, contendo o gradiente
+            na direção X dos frames contidos em stacked_Frames_list
+        stacked_gradient_y_list: uma lista de listas, contendo o gradiente
+            na direção Y dos contidos em stacked_Frames_list
+    """
+    stacked_gradient_x_list = [[cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5) \
+                                for frame in stacked_frames] \
+                                for stacked_frames in stacked_frames_list]  
+    stacked_gradient_y_list = [[cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5) \
+                                for frame in stacked_frames] \
+                                for stacked_frames in stacked_frames_list]
+    return stacked_gradient_x_list, stacked_gradient_y_list
+
+def optical_flow(stacked_frames_list):
+    """ Esta função recebe uma lista com frames empilhados e
+    retorna listas contendo os frames agrupados em
+    stacks, com seus fluxos ópticos na direção X e Y
+
+    Args:
+        stacked_frames_list: uma lista de imagens, correspondendo
+            aos frames de um vídeo
+        
+    Multiple return:
+        stacked_optical_x: uma lista de listas, contendo a posição X
+            do fluxo óptico para cada frame
+        stacked_optical_y: uma lista de listas, contendo a posição Y
+            do fluxo óptico para cada frame
+    """    
+    corner_detect_params = dict( maxCorners = 100,
+                        qualityLevel = 0.3,
+                        minDistance = 7,
+                        blockSize = 7 )
+    lk_params = dict( winSize  = (15,15),
+                    maxLevel = 0,
+                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    
+    stacked_optical_x, stacked_optical_y = []
+
+
+    prev_frame = cv2.cvtColor(stacked_frames_list[0][0], cv2.COLOR_RGB2GRAY)
+    p0 = cv2.goodFeaturesToTrack(prev_frame, mask = None, **corner_detect_params)
+
+    x = []
+    y = []
+
+    # primeiro stack
+    for i in range(len(stacked_frames_list[0]-1)):
+      frame = cv2.cvtColor(stacked_frames_list[0][i+1])
+
+      p1, st, err = cv2.calcOpticalFlowPyrLK(prev_frame, frame, p0, None, **lk_params)
+      
+      good_new = p1[st==1] # Select good points
+
+      x_new, y_new = p1.ravel()
+
+      x.append(x_new)
+      y.append(y_new)
+      
+      prev_frame = frame.copy()
+      p0 = good_new.reshape(-1,1,2)
+
+    
+    stacked_optical_x.append(x)
+    stacked_optical_y.append(y)
+
+    # stacks restantes
+    for i in range(1, len(stacked_frames_list)):
+      x = []
+      y = []
+      for stacked_frames in stacked_frames_list[i]:
+        frame = cv2.cvtColor(stacked_frames_list[0][i+1])
+
+        p1, st, err = cv2.calcOpticalFlowPyrLK(prev_frame, frame, p0, None, **lk_params)
+      
+        good_new = p1[st==1] # Select good points
+
+        x_new, y_new = p1.ravel()
+
+        x.append(x_new)
+        y.append(y_new)
+
+        prev_frame = frame.copy()
+        p0 = good_new.reshape(-1,1,2)
+
+      stacked_optical_x.append(x)
+      stacked_optical_y.append(y)
+
---
+++ src/utils/utils.py
@@ -0,0 +1,174 @@
+''' Contém métodos de uso geral
+@package utils
+
+@author Daniel Kim & Igor Nakamura
+@date Date: 2020-04-17
+
+@verbatim
+Funções presente no módulo: 
+
+initialize_weizmann  Obtém os caminhos dos arquivos de vídeo do dataset Weizmann 
+initialize_kth       Obtém os caminhos dos arquivos de vídeo do dataset KTH
+create_actions_list  Obtém a lista de ações referentes a um dataset específico
+create_names_list    Obtém a lista dos nomes das pessoas que executam as ações
+get_filepaths        Obtém os caminhos para os arquivos de video de um dataset
+get_person_filepaths Obtém os caminhos dos arquivos referentes a uma pessoa
+
+@endverbatim 
+'''
+
+# -----------------------------
+# Standard library dependencies
+# -----------------------------
+import re
+from typing import List, Dict
+
+# -------------------
+# Third-party imports
+# -------------------
+import cv2
+import glob
+
+def initialize_weizmann():
+    """ Obtém um dicionário de dicionários referentes ao dataset Weizmann 
+    como descrito em get_filepaths
+
+    @return Um dicionário de dicionários
+    """
+    path_weiz = r'Weizmann/'
+    person_weiz = 'daria'
+    action_weiz = 'bend'
+    names_list_weiz = create_names_list(path_weiz, action_weiz)
+    actions_list_weiz = create_actions_list(path_weiz, person_weiz)    
+    return(get_filepaths(path_weiz, names_list_weiz, actions_list_weiz))
+
+def initialize_kth():
+    """ Obtém um dicionário de dicionários referentes ao dataset KTH 
+    como descrito em get_filepaths
+
+    @return Um dicionário de dicionários
+    """
+    path_kth = r'KTH/'
+    person_kth = 'person01'
+    action_kth = 'boxing_d1_uncomp'
+    names_list_kth = create_names_list(path_kth, action_kth)
+    actions_list_kth = create_actions_list(path_kth, person_kth)            
+    return(get_filepaths(path_kth, names_list_kth, actions_list_kth))
+
+def create_names_list(path, action):
+    """ Obtém a lista dos nomes das pessoas que executam as ações 
+    de um dataset específico 
+
+    Args:
+        path: caminho para para a pasta desejada. Ex: 'Weizmann/' ou 'KTH/'
+        action: nome da ação. Ex: 'bend' ou 'handclapping_d4_uncomp'
+
+    @return names_list: lista dos nomes das pessoas de um dataset. 
+        Como todas as pessoas executam o mesmo conjunto de ações, essa 
+        variável contêm todos os nomes das pessoas do dataset.
+    
+    Examples:
+        >>> names_list = ['daria', 'shahar', 'lena', 'lyova', etc]
+        >>> names_list = ['person01', 'person02', 'person03', etc]
+    """
+    names_list = []
+    filenames = glob.glob(path+'*'+action+'*')
+    pattern = re.compile("\/(.*?)\_")
+    names_list = [re.search(pattern, filename).group(1) for filename in filenames]  
+    return names_list
+
+def create_actions_list(path, person):
+    """ Obtém a lista de ações referentes a um dataset específico 
+
+    Args:
+        path: caminho para para a pasta desejada. Ex: 'Weizmann/' ou 'KTH/'
+        person: pessoa que executa a ação. Ex: 'daria' ou 'person01'
+
+    @return actions_list: lista de ações referentes a uma pessoa do dataset.
+        Como uma única pessoa executa todas as ações do dataset, essa variável 
+        contêm todas as ações do dataset.
+
+    Examples:
+        >>> actions_list = ['bend', 'wave1', 'wave2', 'walk', 'skip', etc]
+        >>> actions_list = ['handclapping_d4_uncomp', 'boxing_d3_uncomp', etc]
+    """
+    actions_list = []
+    filenames = glob.glob(path+person+'*')
+    pattern = re.compile("\_(.*?)\.")
+    actions_list = [re.search(pattern, filename).group(1) for filename in filenames]
+    return actions_list
+
+def get_filepaths(path, names_list, actions_list):
+    """ Obtém os caminhos para os arquivos de video de um dataset específico
+
+    Args: 
+        path: caminho para para a pasta desejada. Ex: 'Weizmann/' ou 'KTH/'
+        names_list: lista com os nomes das pessoas que executam a ação. Ex: 
+            'daria' para Weizmann e 'person01'para KTH
+        actions_list: lista com as ações. Ex: 'bend', 'walk', etc para Weizmann
+            e 'walking_d1_uncomp', 'person23_boxing_d2_uncomp' para KTH
+
+    @return filepaths: dicionário de dicionários cujos 'keys' do dicionário 
+        mais externo são os nomes das pessoas. Os 'keys' dos dicionários mais
+        internos são as ações associadas a uma pessoa específica. Por fim,
+        os 'values' do dicionário são o caminho completo para o arquivo de vídeo.
+
+    Examples: 
+        >>> print(filepaths['daria']['bend']) 
+        'Weizmann/daria_bend.avi'
+        >>> print(filepaths['person01']['boxing_d1_uncomp']) 
+        'KTH/person01_boxing_d1_uncomp'
+    """
+
+    person_actions_regex_dicts = {}  
+    filepaths = {}
+    for name in names_list:
+        person_actions_regex_dicts[name] = \
+            create_actions_regex_dict(name, actions_list)
+        filepaths[name] = get_person_filepaths(path, 
+                                               person_actions_regex_dicts[name])
+    return filepaths
+
+def create_actions_regex_dict(name, actions_list):
+    """ Cria um dicionário de regex para buscar os nomes dos arquivos desejados
+
+    Args:
+        name: nome de uma pessoa. Ex: 'daria' ou 'person01'
+        actions_list: lista de ações. Já descrito na função get_filepaths
+
+    @return actions_regex_dict: dicionário cujos 'keys' são as ações contidas em 
+            actions_list e cujos 'values' são regex que serão usados para
+            procurar o nome do arquivo de vídeo
+
+    Examples:
+        >>> print(actions_regex_dict['bend']) 
+        r'daria_bend*'
+        >>> print(actions_regex_dict['running_d3_uncomp']) 
+        r'person25_running_d3_uncomp*'
+    """
+    actions_regex_dict = {}
+    for action in actions_list:
+        actions_regex_dict[action] = r'%s_%s*' % (name,action)
+    return actions_regex_dict
+
+def get_person_filepaths(path, actions_regex_dict):
+    """ Obtém os caminhos dos arquivos a partir do regex_dict recebido.
+
+    Args:
+        path: caminho para para a pasta desejada. Ex: 'Weizmann/' ou 'KTH/'
+        actions_regex_dict: idém ao descrito em 'create_actions_regex_dict'
+
+    @return filepaths: dicionário cujos 'keys' são as ações e cujos 'values'
+            são os caminhos dos arquivos de vídeo contendo as ações 
+            de uma pessoa específica
+
+    Examples:
+        >>> filepaths['bend'] = 'Weizmann/daria_bend.avi'
+        >>> filepaths['boxing_d1_uncomp'] = 'KTH/person01_boxing_d1_uncomp'
+    """
+    filepaths = {}
+    for key in actions_regex_dict:
+        filepaths[key] = glob.glob(path+actions_regex_dict[key])
+        if filepaths[key]:
+            filepaths[key] = filepaths[key][0]
+    return filepaths
