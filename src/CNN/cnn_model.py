6ea010498f2a0c692791fbe8ca032ef051bb206c
Daniel Kim <daniel.jin.kim@usp.br>
2020-05-29 20:54:51 -0300

    Added tanh activation

--- src/CNN/cnn_model.py
+++ src/CNN/cnn_model.py
@@ -6,7 +6,7 @@ def get_model(input_shape, num_classes):
     model = Sequential([
         Conv3D(10, (9, 7, 3), input_shape=input_shape, activation='tanh', padding='valid', name='conv_1'),
         MaxPooling3D((3,3,1), padding='valid', name='pool_1'),
-        Conv3D(30, (7,7,3), padding='valid', name='conv_2'),
+        Conv3D(30, (7,7,3), activation='tanh', padding='valid', name='conv_2'),
         MaxPooling3D((3,3,1), padding='valid', name='pool_2'),
         Reshape((6, 4, 150), name='reshape'),
         Conv2D(128, (6,4), padding='valid', activation='tanh', name='conv_3'),
