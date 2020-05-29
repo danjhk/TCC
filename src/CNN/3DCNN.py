fd603c1aaa03bfba8cc9c59dc7b3dfde53b77578
Daniel Kim <daniel.jin.kim@usp.br>
2020-05-29 16:00:17 -0300

    Added Keras model and training

---
+++ src/CNN/3DCNN.py
@@ -0,0 +1,25 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Conv3D, MaxPooling3D, Flatten, Dropout, Reshape
+
+def get_model(input_shape, num_classes):
+    model = Sequential([
+        Conv3D(10, (9, 7, 3), input_shape=input_shape, activation='tanh', padding='valid', name='conv_1'),
+        MaxPooling3D((3,3,1), padding='valid', name='pool_1'),
+        Conv3D(30, (7,7,3), padding='valid', name='conv_2'),
+        MaxPooling3D((3,3,1), padding='valid', name='pool_2'),
+        Reshape((6, 4, 150), name='reshape'),
+        Conv2D(128, (6,4), padding='valid', activation='tanh', name='conv_3'),
+        Flatten(),
+        Dense(num_classes, activation='softmax', name='dense-softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+def train_network(model, X, y):
+    history = model.fit(X[np.newaxis,...],
+                        y[np.newaxis,...],
+                        verbose=1,
+                        epochs=1)
+    return model, history
